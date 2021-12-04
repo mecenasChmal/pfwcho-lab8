4b4462878697febdb62be1266eef67d35d6332b6
mecenaschmal <jchmal71@gmail.com>
2021-12-04 22:29:35 +0100

    fibonacci sequence + travis update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(fibonacci(inputs.k));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 123 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label for="k">Podaj k: </label>
+          <input type="number" min="0" max="40" 
+          name="k" value={inputs.k || ""} onChange={handleChange} />
+          <input type="submit" value="Oblicz" />
+        </form>
+        
     </div>
   );
+}
+
+function fibonacci(k) {
+  if (k==0) return 0;
+  if (k==1) return 1;
+  return fibonacci(k-1)+fibonacci(k-2);
 }
 
 export default App;
